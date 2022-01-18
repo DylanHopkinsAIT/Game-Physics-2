b9c1c8060a31e4397ef1f479af8a26b31e00a433
Dylan Hopkins <90385685+DylanHopkinsAIT@users.noreply.github.com>
2022-01-18 17:30:16 +0000

    Basics
    Level Designed
    Collisions
    Character Movement

---
+++ Lab 1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+
+    [SerializeField] private float speed = 3.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // For Physics
+    void FixedUpdate()
+    {
+        // Players moves up 
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+    }
+
+}
---
+++ Lab 1/Assets/Scripts/coinsTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinsTrigger : MonoBehaviour
+{
+
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player hit: " + collision.name);
+
+        Destroy(collision.gameObject);
+        score += 1;
+
+    }
+
+
+
+}
